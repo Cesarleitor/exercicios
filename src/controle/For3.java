5122592bd5944710456fceb3464babb8977ef5d3
Cesar Augusto <100009078+Cesarleitor@users.noreply.github.com>
2025-06-21 22:18:33 -0300

    21/06/25 22:18

--- src/controle/For3.java
+++ src/controle/For3.java
@@ -1,11 +1,16 @@
 package controle;
 
 public class For3 {
-
-    public static void main(String[] args) {
-        
-        for (int i = 0; i < 10; i++) { System.out.println(i);}
-        
-    }
-
+	
+	public static void main(String[] args) {
+		
+		int i = 0;
+		for (; i <10; i++) {
+			System.out.println(i);
+		}
+		
+		System.out.println("Saiu do for..");
+		System.out.println(i);
+         
+} 
 }
